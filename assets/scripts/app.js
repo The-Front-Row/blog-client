4d896032e283c25e12b1f9b8fa1126b8f2fb146e
Henry Minicucci <56238337+henrymini@users.noreply.github.com>
2020-01-07 15:50:34 -0500

    Revert "Post view handlebars"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,11 @@
 'use strict'
-const commonUi = require('./common/ui')
-const authEvents = require('./auth/events')
-const postViewEvents = require('./post-view/events')
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+
+// use require without a reference to ensure a file is bundled
+// require('./example')
 
 $(() => {
-  commonUi.init()
-  authEvents.addHandlers()
-  postViewEvents.addHandlers()
+  // your JS code goes here
 })
