f646500540a7e1cb14b0122774deb64199f7a9e6
RWPicklechips <hminicucci@gmail.com>
2020-01-07 15:24:59 -0500

    Auth nav bar
    - Added a new handlebars template for the authorized nav bar
    - Implemented loadNavAuth() in onSignInSuccess

--- assets/scripts/nav/ui.js
+++ assets/scripts/nav/ui.js
@@ -1,10 +1,17 @@
-const navPublic = require('../templates/navPublic.handlebars')
+const navPublic = require('../templates/NavPublic.handlebars')
+const navAuth = require('../templates/NavAuth.handlebars')
 
 const loadNavPublic = () => {
   const navPublicHtml = navPublic()
   $('#nav').html(navPublicHtml)
 }
 
+const loadNavAuth = () => {
+  const navAuthHtml = navAuth()
+  $('#nav').html(navAuthHtml)
+}
+
 module.exports = {
-  loadNavPublic
+  loadNavPublic,
+  loadNavAuth
 }
