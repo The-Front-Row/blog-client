7892023483bb44ec34a6ec3a43a5090acc326bd7
michaelchun <michael.chun03@gmail.com>
2020-01-08 14:48:39 -0500

    Home handlebars now lists real posts

--- assets/scripts/common/api.js
+++ assets/scripts/common/api.js
@@ -1,6 +1,5 @@
 const config = require('../config')
 
-// TODO: unsure if we need token here, remove if it's causing issues
 const getUserHome = () => {
   return $.ajax({
     url: config.apiUrl + '/posts',
