237a85d1f56858c35b8eccbc5fcbb999fe4667ac
michaelchun <michael.chun03@gmail.com>
2020-01-09 15:54:25 -0500

    Turn change pw into its own page and link to it
    Co-authored-by: Spencer spencer.r1@outlook.com

--- assets/scripts/nav/ui.js
+++ assets/scripts/nav/ui.js
@@ -1,6 +1,7 @@
 const navPublic = require('../templates/NavPublic.handlebars')
 const navAuth = require('../templates/NavAuth.handlebars')
 const navForm = require('../templates/SignUpForm.handlebars')
+const changePasswordForm = require('../templates/ChangePasswordForm.handlebars')
 
 const loadNavPublic = () => {
   const navPublicHtml = navPublic()
@@ -17,8 +18,14 @@ const loadNavSignUpForm = () => {
   $('#content').html(navSignUpForm)
 }
 
+const loadChangePasswordForm = () => {
+  const changePasswordHtml = changePasswordForm()
+  $('#content').html(changePasswordHtml)
+}
+
 module.exports = {
   loadNavSignUpForm,
   loadNavPublic,
-  loadNavAuth
+  loadNavAuth,
+  loadChangePasswordForm
 }
