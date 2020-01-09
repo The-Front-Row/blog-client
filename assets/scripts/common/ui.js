66759c60439fb8fed644c53d70c9dcb905d782d4
michaelchun <michael.chun03@gmail.com>
2020-01-09 17:43:02 -0500

    add css classes to post card

--- assets/scripts/common/ui.js
+++ assets/scripts/common/ui.js
@@ -2,6 +2,7 @@ const home = require('../templates/Home.handlebars')
 
 // TODO: when GET to /posts is working, update this function to accept data from the api and throw it into handlebars
 const loadHome = data => {
+  console.warn(data)
   const homeHtml = home({ posts: data.posts })
   $('#content').html(homeHtml)
 }
