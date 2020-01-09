d4f6f5192d4d571593415f5dbdddc2b8c80bbf12
michaelchun <michael.chun03@gmail.com>
2020-01-08 20:23:04 -0500

    Comments section now displays real data

--- assets/scripts/post-view/ui.js
+++ assets/scripts/post-view/ui.js
@@ -5,15 +5,6 @@ const loadPostView = data => {
   $('#content').html(postViewHtml)
 }
 
-// when you add a comment
-const addCommentSuccess = () => {
-  // reset the form
-  $('form').trigger('reset')
-  // then load the PostView again, so you see the new comment
-  loadPostView()
-}
-
 module.exports = {
-  loadPostView,
-  addCommentSuccess
+  loadPostView
 }
